8410a0d01cbb2363a37ff5c53af74063615be253
James Bannan <james@bannan.com.au>
2016-08-09 09:37:13 +1000

    Updated Assert-SecureResourceGroupRbac to support modified Get-AzureRegion function

--- functions/Assert-SecureResourceGroupPolicyLocation.ps1
+++ functions/Assert-SecureResourceGroupPolicyLocation.ps1
@@ -1,20 +1,27 @@
 ﻿<#
+
 .SYNOPSIS
     Applies an ARM policy on a Resource Group to prevent deployment of resources in other geo-political regions.
+
 .DESCRIPTION
     The Assert-SecureResourceGroupPolicyLocation cmdlet checks for the existence of an ARM policy which prevents deployment of resources outside the geopolitical region in which the Resource Group is located.
 
     For example, if the Resource Group is in Australia Southeast, a policy will be created which only allows resources within the Resource Group to be deployed to Australia Southeast and Australia East.
 
     The policy is only created if the Resource Group has a tag called 'policy-location' with a value of 'enabled'.
+
 .EXAMPLE
     C:\PS> $resourceGroups = Find-AzureRmResourceGroup -Tag @{ Name='policy-location'; Value='enabled' } | Get-MicrosoftAzureDatacenterIPRange -resourceGroups $resourceGroups
     Finds all Resource Groups in the current subscription with the resource tag 'policy-location':'enabled', and assigns an ARM policy to prevent deployment of resources outside the Resource Group's geo-political region.
+
 .INPUTS
     Can take Azure Resource Group properties from Find-AzureRmResourceGroup from the pipeline.
+
 .OUTPUTS
     None.
+
 #>
+
 function Assert-SecureResourceGroupPolicyLocation
 {
     [CmdletBinding()]
@@ -37,7 +44,7 @@ function Assert-SecureResourceGroupPolicyLocation
     {
         foreach($resourceGroup in $resourceGroups){
             $resourceGroupName = $resourceGroup.name
-            $resourceGroupRegion = ((Get-AzureRegion -resourceGroupLocation $resourceGroup.location).Region).ToLower()
+            $resourceGroupRegion = ((Get-AzureRegion -location $resourceGroup.location).Region).ToLower()
             $policyName = 'resource-group-location' + '-' + $resourceGroupRegion
             $azureLocations = Get-AzureRmLocation | Where-Object {$_.Location -like "$resourceGroupRegion*" -or $_.Location -like "*$resourceGroupRegion"}
             if((Get-AzureRmPolicyDefinition | Where-Object {$_.Name -eq $policyName}) -eq $null){
